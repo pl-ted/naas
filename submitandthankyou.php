559dc0f9efd3dab4add02ee6aca6f376d298e48d
Ted Cheung <61783572+pl-ted@users.noreply.github.com>
2020-12-18 17:42:41 +0000

    Add files via upload

---
+++ submitandthankyou.php
@@ -0,0 +1,313 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+
+    <meta charset="utf-8">
+    <title>National Annuity Advisory Service</title> 
+    <meta name="description" content="National Annuity Advisory Service. Fill In Our 30 Second Form To Get Started."> 
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+    <link rel="shortcut icon" href="https://storage.googleapis.com/site-pl/4f78165e-73f1-456b-9bd0-0dbfb29b1970.png"> 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
+<link href="https://getbootstrap.com/docs/4.3/examples/carousel/carousel.css" media="screen" type="text/css" rel="stylesheet">
+<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" media="screen" type="text/css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" type="text/javascript"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="https://widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" type="text/javascript"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" media="screen" type="text/css" rel="stylesheet">
+<script src="./localization.a7299b17a1a573f912c3aaa8781eef49.js" type="text/javascript"></script><style type="text/css">body {
+    padding-top: 0rem;
+    }
+
+.container {
+    padding-left: 0px;
+    padding-right: 0px;
+}
+.navbar-light .navbar-nav .nav-link {
+    font-size: 20px;
+}
+
+
+.logoStyle {
+    width: 440px;
+    height: 99px;
+}
+
+@media (max-width: 576px) {
+    
+}
+
+
+@media (max-width: 768px) {
+    .header {
+        padding-top: 0px;
+        padding-bottom: 0px;
+    }
+    .logoStyle {
+        width: 330px; 
+        height: 80px;
+    }
+}
+
+
+@media (max-width: 992px) { 
+
+ }
+
+
+@media (max-width: 1200px) {
+   
+}
+.footerAboveSection {
+    padding: 90px 0;
+    background-color: #f7f7f7;
+}
+
+.footerAboveContainer {
+    width: 100%;
+}
+
+.footerAboveContainer h3 {
+    color: #31455F;
+    text-align: center;
+    line-height: 52px;
+    font-size: 42px;
+    width: 85%;
+    margin: 0 auto;
+}
+
+footer {
+    background-color: #222;
+    padding: 30px 0;
+}
+
+.footerWrapper {
+    width: 100%;
+}
+
+.footerWrapper p {
+    font-weight: 400;
+    font-style: normal;
+    font-size: 16px;
+    color: #fff;
+    line-height: 24px;
+    text-align: center;
+    width: 74%;
+    margin: 0 auto;
+}</style></head>
+<body class="col-12">
+
+
+    <header style="background-color: white;">
+    <div class="row">
+        <div class="col-12 col-sm-10 offset-sm-1 d-flex flex-wrap justify-content-center text-center form-inline">
+            <div class="col-sm-12 col-md-6">
+                <img src="https://storage.googleapis.com/site-pl/298bb42b-99db-42ce-8b6f-e094442cadb6.png" class="logoStyle">
+            </div>
+            <div class="col-sm-12 col-md-6">
+                <!-- TrustBox widget - Micro Star --> <div class="trustpilot-widget" data-locale="en-GB" data-template-id="5419b732fbfb950b10de65e5" data-businessunit-id="5d5131486a32170001c259b5" data-style-height="24px" data-style-width="100%" data-theme="light"> <a href="https://uk.trustpilot.com/review/pension-advisers.co.uk" target="_blank" rel="noopener">Trustpilot</a> </div> <!-- End TrustBox widget -->
+            </div>
+        </div>
+    </div>
+
+</header>
+    
+
+
+<div class="container-fluid bg-light">
+    <div class="container featurette-heading text-dark text-center pt-0 pb-0 mt-3 pt-md-1 pb-md-5 pl-md-5 pr-md-5 ">    
+        
+            <div class=" h3 font-weight-bold p-2 p-md-3 col-10 offset-1 col-sm-10 offset-sm-1 col-md-10 offset-md-1 ">
+            Thank you, we have received your enquiry. 
+        </div>
+            <div class=" h5 font-weight-bold p-2 p-md-3 col-10 offset-1 col-sm-10 offset-sm-1 col-md-10 offset-md-1 ">
+           Someone will be in touch with you soon to check some additional details so we can get you the best Annuity quote possible.
+        </div>
+        <div class=" h5 font-weight-bold p-2 p-md-3 col-10 offset-1 col-sm-10 offset-sm-1 col-md-10 offset-md-1 ">
+           And remember, you only get once chance to buy your Annuity. Make sure you answer the call and ensure you maximise your income in retirement. The
+           quote is FREE and without obligation. If you do choose to proceed with our quote, we offer some of the cheapest fees available. 
+        </div>
+        <div class=" h5 font-weight-bold p-2 p-md-3 col-10 offset-1 col-sm-10 offset-sm-1 col-md-10 offset-md-1 ">
+           Don't regret not taking this call, shop around and check the deal from your provider before you make a decision. 
+        </div>
+    </div>
+</div>
+
+<div id="expert" class="container-fluid" style="background-color: #31455F; padding-bottom: 5%;">
+<div class="container">
+
+<div class="row featurette justify-content-center p-3 mb-4"> 
+  <div class="h3 featurette-heading font-weight-bold" style="padding-bottom:20px; color: white;">For now, just put the kettle on and wait for your call...</div>
+      
+
+    <div class="card-deck text-center">
+    <div class="card" style="border: none; border-radius: 0rem;"> 
+
+        <div class="card-body">
+
+        <p class="card-text">We have texted and emailed you details of your matched broker. They will be in touch soon, or you can call them now if you wish.</p>
+        </div>
+    </div>
+    <div class="card" style="border: none; border-radius: 0rem;">
+        <div class="card-body">
+
+        <p class="card-text">Have your FREE phone consultation with your broker to establish your needs in more detail.</p>
+        </div>
+    </div>
+    <div class="card" style="border: none; border-radius: 0rem;">
+        <div class="card-body">
+
+        <p class="card-text">If you are unsure at any time, please call us again and we'll endeavour to resolve any issue, or find you an alternative broker free of charge.</p>
+        </div>
+    </div>
+    </div>
+
+
+</div> <!-- row featurette -->
+
+
+</div></div>
+    <footer class="pt-4  pt-md-5 border-top" style="color: white;">
+
+<div class="container-fluid p-1 pl-md-5 pr-md-5">
+      <div class="row d-flex flex-wrap-reverse">
+        <div class="col-10 offset-1 col-sm-9 offset-sm-2 col-md-5 offset-md-1 col-lg-3 offset-lg-1">
+          <h5>National Annuity Advisory Service</h5>
+          <ul class="list-unstyled text-small">
+            <li style="word-wrap: break-word;">NationalAnnuityAdvisoryService.co.uk is a trading name of Performance Leads Ltd, Company Number: 10720622, VAT Number: 276316588, Registered Office: Haggs Farm, Unit 5, Thwaites Barn, Haggs Rd, Harrogate HG3 1EQ.</li>
+          </ul>
+        </div>
+        <div class="col-10 offset-1 col-sm-9 offset-sm-2 col-md-5 offset-md-1 col-lg-3 offset-lg-1">
+          <h5>Performance Leads Limited</h5>
+          <ul class="list-unstyled text-small">
+            <li>Performance Leads Limited is registered with the Information Commissioner&rsquo;s Office, Registration Number ZA247374, <a href="/privacy.html" style="color:#ffffff"><u>Privacy.</u></a> Tel: <a href="tel:01423423065" style="color:#ffffff"><u>01423 423065</u></a>, Email: <a href="mailto:support@performance-leads.co.uk" style="color:#ffffff"><u>support@performance-leads.co.uk</u></a> &copy; 2015 - 2020 Performance Leads Ltd</li>
+          </ul>
+        </div>
+        <div class="mb-4 col-10 offset-1 col-sm-9 offset-sm-2 col-md-10 offset-md-1 col-lg-3 offset-lg-1" style="background-color: #31455F; border-radius: 5px; padding: 3%; align-self: center;">
+          <h5>
+            If you are a Financial Advisor and would be interested in working with us,
+
+            <a style="color:white" href="https://try.performance-leads.co.uk/try/"><u>Click here to find out more.</u></a>   
+          </h5>
+        </div>
+      </div>
+    </div> <!-- container-fluid -->
+  </footer>
+
+
+<div class="col-12" style="background-color: #212529; word-wrap: break-word;">
+
+    <?php
+if (!empty($_POST)) {
+    header("access-control-allow-credentials:true");
+    header("access-control-allow-headers:Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token");
+    header("access-control-allow-methods:POST, GET, OPTIONS");
+    header("access-control-allow-origin:".$_SERVER['HTTP_ORIGIN']);
+    header("access-control-expose-headers: AMP-Access-Control-Allow-Source-Origin");
+    header("amp-access-control-allow-source-origin:https://".$_SERVER['HTTP_HOST']);
+    header("Content-Type: application/json");
+    $typeOfAdvice = isset($_POST['typeOfAdvice']) ? $_POST['typeOfAdvice'] : '';
+    $email = isset($_POST['email']) ? $_POST['email'] : '';
+    $postcode = isset($_POST['postcode']) ? $_POST['postcode'] : '';
+    $firstName = isset($_POST['firstName']) ? $_POST['firstName'] : '';
+    $lastName = isset($_POST['lastName']) ? $_POST['lastName'] : '';
+    $phone = isset($_POST['phone']) ? $_POST['phone'] : '';
+    $gclid = isset($_POST['gclid']) ? $_POST['gclid'] : '';
+    $pl_matchtype = isset($_POST['pl_matchtype']) ? $_POST['pl_matchtype'] : '';
+    $pl_campaignid = isset($_POST['pl_campaignid']) ? $_POST['pl_campaignid'] : '';
+    $pl_adgroupid = isset($_POST['pl_adgroupid']) ? $_POST['pl_adgroupid'] : '';
+    $pl_device = isset($_POST['pl_device']) ? $_POST['pl_device'] : '';
+    $pl_devicemodel = isset($_POST['pl_devicemodel']) ? $_POST['pl_devicemodel'] : '';
+    $pl_creative = isset($_POST['pl_creative']) ? $_POST['pl_creative'] : '';
+    $pl_keyword = isset($_POST['pl_keyword']) ? $_POST['pl_keyword'] : '';
+    $pl_adposition = isset($_POST['pl_adposition']) ? $_POST['pl_adposition'] : '';
+    $pl_network = isset($_POST['pl_network']) ? $_POST['pl_network'] : '';
+    $pl_searchterm = isset($_POST['pl_searchterm']) ? $_POST['pl_searchterm'] : '';
+    $pl_page = isset($_POST['pl_page']) ? $_POST['pl_page'] : '';
+    $pl_feeditemid = isset($_POST['pl_feeditemid']) ? $_POST['pl_feeditemid'] : '';
+    $pl_targetid = isset($_POST['pl_targetid']) ? $_POST['pl_targetid'] : '';
+    $pl_loc_physical_ms = isset($_POST['pl_loc_physical_ms']) ? $_POST['pl_loc_physical_ms'] : '';
+    $pl_loc_interest_ms = isset($_POST['pl_loc_interest_ms']) ? $_POST['pl_loc_interest_ms'] : '';
+    $pl_siteid = isset($_POST['pl_siteid']) ? $_POST['pl_siteid'] : '';
+    $pl_leadgroupid = isset($_POST['pl_leadgroupid']) ? $_POST['pl_leadgroupid'] : '';
+    $pl_associatedsite = isset($_POST['pl_associatedsite']) ? $_POST['pl_associatedsite'] : '';
+ 
+    $post = array(
+        'email' => $email,
+        'typeOfAdvice' => $typeOfAdvice,
+        'postcode'  => $postcode,
+        'firstName' => $firstName,
+        'lastName' => $lastName,
+        'phone' => $phone,
+        'gclid' => $gclid,
+        'pl_matchtype' => $pl_matchtype,
+        'pl_campaignid' => $pl_campaignid,
+        'pl_adgroupid' => $pl_adgroupid,
+        'pl_device' => $pl_device,
+        'pl_devicemodel' => $pl_devicemodel,
+        'pl_creative' => $pl_creative,
+        'pl_keyword' => $pl_keyword,
+        'pl_adposition' => $pl_adposition,
+        'pl_network' => $pl_network,
+        'pl_searchterm' => $pl_searchterm,
+        'pl_page' => $pl_page,
+        'pl_feeditemid' => $pl_feeditemid,
+        'pl_targetid' => $pl_targetid,
+        'pl_loc_physical_ms' => $pl_loc_physical_ms,
+        'pl_loc_interest_ms' => $pl_loc_interest_ms,
+        'pl_siteid' => $pl_siteid,
+        'pl_leadgroupid' => $pl_leadgroupid,
+        'pl_associatedsite' => $pl_associatedsite,
+       
+    );
+    header("Content-Type: application/json");
+    echo json_encode($post);
+    $post_data['email'] = urlencode($_POST['email']);
+    $post_data['typeOfAdvice'] = urlencode($_POST['typeOfAdvice']);
+    $post_data['postcode'] = urlencode($_POST['postcode']);
+    $post_data['firstName'] = urlencode($_POST['firstName']);
+    $post_data['lastName'] = urlencode($_POST['lastName']);
+    $post_data['phone'] = urlencode($_POST['phone']);
+    $post_data['gclid'] = urlencode($_POST['gclid']);
+    $post_data['pl_matchtype'] = urlencode($_POST['pl_matchtype']);
+    $post_data['pl_campaignid'] = urlencode($_POST['pl_campaignid']);
+    $post_data['pl_adgroupid'] = urlencode($_POST['pl_adgroupid']);
+    $post_data['pl_device'] = urlencode($_POST['pl_device']);
+    $post_data['pl_devicemodel'] = urlencode($_POST['pl_devicemodel']);
+    $post_data['pl_creative'] = urlencode($_POST['pl_creative']);
+    $post_data['pl_keyword'] = urlencode($_POST['pl_keyword']);
+    $post_data['pl_adposition'] = urlencode($_POST['pl_adposition']);
+    $post_data['pl_network'] = urlencode($_POST['pl_network']);
+    $post_data['pl_searchterm'] = urlencode($_POST['pl_searchterm']);
+    $post_data['pl_page'] = urlencode($_POST['pl_page']);
+    $post_data['pl_feeditemid'] = urlencode($_POST['pl_feeditemid']);
+    $post_data['pl_targetid'] = urlencode($_POST['pl_targetid']);
+    $post_data['pl_loc_physical_ms'] = urlencode($_POST['pl_loc_physical_ms']);
+    $post_data['pl_loc_interest_ms'] = urlencode($_POST['pl_loc_interest_ms']);
+    $post_data['pl_siteid'] = urlencode($_POST['pl_siteid']);
+    $post_data['pl_leadgroupid'] = urlencode($_POST['pl_leadgroupid']);
+    $post_data['pl_associatedsite'] = urlencode($_POST['pl_associatedsite']);
+    foreach($post_data as $key => $value) {
+        $post_items[] = $key.
+        '='.$value;
+    }
+    $post_string = implode('&', $post_items);
+    $curl_connection = curl_init('https://hooks.zapier.com/hooks/catch/2162551/octd4ym/');
+    curl_setopt($curl_connection, CURLOPT_CONNECTTIMEOUT, 30);
+    curl_setopt($curl_connection, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($curl_connection, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($curl_connection, CURLOPT_FOLLOWLOCATION, 1);
+    curl_setopt($curl_connection, CURLOPT_POSTFIELDS, $post_string);
+    curl_exec($curl_connection);
+    curl_close($curl_connection);
+    header("Content-Type: text/html; charset=utf-8");
+
+
+}
+?>  
+
+</div>
+
+</body>
+</html>
